041a8055cbc9dd09ad7ab327cd5b2db2395fb3cd
jeremy black <jrebla85@yahoo.com>
2021-07-24 01:46:53 -0500

    Updated form functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+
 function App() {
+
+  const [form, setForm] = useState({
+    username: ''
+  })
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  const changeHandler = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Form onSubmit={submitHandler} onChange={changeHandler} form={form} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = ({ onChange, onSubmit}) => {
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+            
+            </form>
+        </div>
+    )
+}
+
+export default Form
